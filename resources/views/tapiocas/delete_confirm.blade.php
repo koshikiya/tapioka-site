6681bcca45bc457fd7b33ae6a74df161bceff049
koshikiya <rhxck.lv@gmail.com>
2019-11-14 11:15:52 +0000

    add css

--- resources/views/tapiocas/delete_confirm.blade.php
+++ resources/views/tapiocas/delete_confirm.blade.php
@@ -1,18 +1,25 @@
+
 @extends('layouts.app')
 
 @section('content')
-    <div class="panel panel-default">
-        <div class="panel-body">
-            <p>アカウントを削除すると、ログインができなくなり、データも全て削除されます。</p>
-            <p>本当に削除してよろしいですか？</p>
-        </div>
-    </div>
-    <div class="col-md-2 col-md-offset-5">
-        {!! Form::open(['route' => ['users.delete', $user->id], 'method' => 'delete']) !!}
-            {!! Form::submit('削除する', ['class' => 'btn btn-danger btn-block']) !!}
-        {!! Form::close() !!}
-    </div>
-    <div class="col-md-2 col-md-offset-5 spacer">
+    <div class="text-center">
         
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+            <h5>＊確認</h5>
+            <div class="confirm">アカウントを削除すると、ログインができなくなり、データも全て削除されます。
+                本当に削除してよろしいですか？</div>
+            
+                
+
+                {!! Form::open(['route' => ['users.delete', $user->id], 'method' => 'delete']) !!}
+                    <div class="info">{!! Form::submit('退会する', ['class' => 'btn btn-default btn-sm']) !!}</div>
+                {!! Form::close() !!}
+            
+          
+
+        </div>
     </div>
 @endsection
