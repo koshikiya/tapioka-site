6f5202704937cebc4c7b1a3bb6e67569a9f02857
koshikiya <rhxck.lv@gmail.com>
2019-11-26 07:29:36 +0000

    login edit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="text-center">
-        <h3>Sign up</h3>
+        <h3>新規登録</h3>
     </div>
 
     <div class="row">
@@ -10,26 +10,26 @@
 
             {!! Form::open(['route' => 'signup.post']) !!}
                 <div class="form-group">
-                    {!! Form::label('name', 'Name') !!}
+                    {!! Form::label('name', '名前') !!}
                     {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
                 </div>
 
                 <div class="form-group">
-                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::label('email', 'メールアドレス') !!}
                     {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
                 </div>
 
                 <div class="form-group">
-                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::label('password', 'パスワード') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
 
                 <div class="form-group">
-                    {!! Form::label('password_confirmation', 'Confirmation') !!}
+                    {!! Form::label('password_confirmation', 'パスワード(確認)') !!}
                     {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
 
-                <div class="info">{!! Form::submit('Sign up', ['class' => 'btn btn-default btn-sm']) !!}</div>
+                <div class="info">{!! Form::submit('登録', ['class' => 'btn btn-default btn-sm']) !!}</div>
             {!! Form::close() !!}
         </div>
     </div>
