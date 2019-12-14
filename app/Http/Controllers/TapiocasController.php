1b0c03727162b32744ef21cbf08dd4e3f023c34b
koshikiya <rhxck.lv@gmail.com>
2019-12-14 12:31:10 +0000

    remove comment

--- app/Http/Controllers/TapiocasController.php
+++ app/Http/Controllers/TapiocasController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Tapioca;
-//use Intervention\Image\ImageManagerStatic as Image;
+
 
 class TapiocasController extends Controller
 {
